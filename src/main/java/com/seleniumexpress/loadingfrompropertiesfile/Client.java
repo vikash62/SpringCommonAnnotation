689f6351694cbc4e2b34011f2f10e80e52fb7b4f
Vikash <vikashpathak041@gmail.com>
2022-08-15 10:08:56 +0530

    first commit

--- src/main/java/com/seleniumexpress/loadingfrompropertiesfile/Client.java
+++ src/main/java/com/seleniumexpress/loadingfrompropertiesfile/Client.java
@@ -1,10 +1,11 @@
 package com.seleniumexpress.loadingfrompropertiesfile;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Client {
  public static void main(String[] args) {
-	 ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+	 ApplicationContext context = new AnnotationConfigApplicationContext(CollegeConfig.class);
 		System.out.println("beans file loaded");
 		
 		 College college =  context.getBean("collegeBean",College.class);
